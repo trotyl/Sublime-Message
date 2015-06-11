824b4f5dd0e187ebaa7867f49f06047feb2ed339
trotyl <trotyl@qq.com>
2015-06-11 19:26:03 +0800

    新建ListsViewModel

---
+++ SublimeMessage/ViewModels/ListsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using SublimeMessage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SublimeMessage.ViewModels
+{
+    public class ListsViewModel
+    {
+        public IEnumerable<User> Users { get; set; }
+        public IEnumerable<Group> Groups { get; set; }
+    }
+}
